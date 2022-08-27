537250d27fb7e485d9ca0d0092a5d29ceba69f5a
sevmarc <marton.sevella@gmail.com>
2022-08-27 17:58:02 +0200

    Formatting, generating html

--- parse_places.py
+++ parse_places.py
@@ -1,7 +1,5 @@
-import osmnx as ox
 import folium
 from geopy.geocoders import Nominatim
-import glob, os
 from pathlib import Path, PosixPath
 
 
@@ -33,7 +31,7 @@ class Location:
         location_name_raw_small = self.location_name_raw.lower()
         start_loc, end_loc = 0, 0
         looking_started = False
-        for i,l in enumerate(location_name_raw_small):
+        for i, l in enumerate(location_name_raw_small):
             if not looking_started:
                 if l in letters:
                     looking_started = True
@@ -41,23 +39,22 @@ class Location:
             else:
                 if l not in (letters + ' '):
                     end_loc = i
-                    break 
+                    break
         place = location_name_raw_small[start_loc:end_loc]
-        
+
         return place
 
-    def get_geolocation(self, nom:Nominatim):
+    def get_geolocation(self, nom: Nominatim):
         get_loc = nom.geocode(self.finalized_location)
-        if get_loc: 
+        if get_loc:
             self.lat_long = [get_loc.latitude, get_loc.longitude]
             return self.lat_long
         else:  # NoneType
             print(f'\'{self.finalized_location}\' was not understood. ')
 
 
-
-def list_filenames(main_path: str='bucketlist/PLACES', 
-                  file_extension: str='md') -> list[PosixPath]:
+def list_filenames(main_path: str = 'bucketlist/PLACES',
+                   file_extension: str = 'md') -> list[PosixPath]:
     """ Lists all files under main_path with file_extension extension """
     results = []
     for path in Path(main_path).rglob(f'*.{file_extension}'):
@@ -71,17 +68,17 @@ def parse_places() -> list[Location]:
     then parses them into clean format, usable for map plotting.
     """
     places_raw = []
-    
+
     for filename in list_filenames():
         with open(filename) as tf:
             lines = tf.readlines()
             filename_noext = filename.stem  # no extension
-        
+
         for ind, line in enumerate(lines):
             if keyword in line:  # the next line contains location info
                 places_raw.append([lines[ind + 1], filename_noext])
-    
-    return [Location(place, country) for place,country in places_raw]
+
+    return [Location(place, country) for place, country in places_raw]
 
 
 def plot_location(locations: list[Location]):
@@ -90,13 +87,13 @@ def plot_location(locations: list[Location]):
     adds any location that was passed as a marker.
     In the future, the marker color could represent
     whether the location has been visited or not.
-    
+
     TODO: exception handling for when the location 
     name is not understood, some sort of warning 
     should be presented to the user (me)
     """
     loc = Nominatim(user_agent="getLoc")
-    
+
     # creating map, around origo, zoom can be reconfigured
     origo = loc.geocode('Budapest, Hungary')
     orig_xy = [origo.latitude, origo.longitude]
@@ -107,6 +104,7 @@ def plot_location(locations: list[Location]):
         if lat_long:
             folium.CircleMarker(location=location.lat_long).add_to(m)
     m.save('bucketlist.html')
+    print(f'Bucketlist map was saved to: bucketlist.html')
 
 
 if __name__ == '__main__':
